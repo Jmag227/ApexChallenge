ccd4b9506be2213229b8590790d6ca0ba908a1ad
Jmag227 <jarom.pro.magalei@gmail.com>
2023-03-22 13:53:54 -0600

    just added the TestData Class

---
+++ ApexChallenges/Models/TestTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ApexChallenges.Models
+{
+    public class TestTable
+    {
+        public int Id { get; set; }
+        public string Namme { get; set; }
+        public string Description { get; set; }
+    }
+}
