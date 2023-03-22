e4ce97221ca5dea25effa83136de53bd10ef6ea4
Jmag227 <jarom.pro.magalei@gmail.com>
2023-03-22 12:26:21 -0600

    init

---
+++ ApexChallenges/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApexChallenges.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
